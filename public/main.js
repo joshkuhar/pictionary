b11f772571327ec9614ab40b59847b7d8dd1ebca
Josh Kuhar <jdkuhar@gmail.com>
2016-10-03 11:08:45 -0400

    update

---
+++ public/main.js
@@ -0,0 +1,33 @@
+var socket = io();
+
+var pictionary = function(){
+	var canvas, context;
+
+	var draw = function(position) {
+		context.beginPath();
+		context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
+		context.fill();
+	};
+
+	canvas = $('canvas');
+	context = canvas[0].getContext('2d');
+	canvas[0].width = canvas[0].offsetWidth;
+	canvas[0].height = canvas[0].offsetHeight;
+	canvas.on('mousemove', function(event) {
+		var offset = canvas.offset();
+		var position = {x: event.pageX - offset.left,
+						y: event.pageY - offset.top};
+		draw(position);
+		socket.emit('drawing', position);
+	});
+	socket.on('drawing', draw);
+};
+
+$(document).ready(function() {
+	pictionary();
+});
+
+// Emit a draw event from your mousemove function to the Socket.IO server.
+// The event should contain the position object as data.
+// In server.js, listen for the draw event, and broadcast it out to all other clients.
+// Listen for the broadcast draw event in public/main.js, and call the draw function when it is received
--- server.js
+++ server.js
@@ -8,6 +8,25 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+io.on('connection', function (socket) {
+	// socket.broadcast.emit('drawing', 'Drawing') {
+	// 	console.log('drawing');
+	// 	socket.on('drawing', function(draw){
+
+	// 	});
+	// };
+
+	socket.on('draw', function(position) {
+        console.log('Received drawing', position);
+        socket.broadcast.emit('drawing', position);
+    });
+});
+
 
 server.listen(process.env.PORT || 8080);
+
+// Emit a draw event from your mousemove function to the Socket.IO server.
+// The event should contain the position object as data.
+// In server.js, listen for the draw event, and broadcast it out to all other clients.
+// Listen for the broadcast draw event in public/main.js, and call the draw function when it is received
 
